6250dd235fe8f75fe5d07a4de535b06efe3ab654
happyjino <wlsgh1128@naver.com>
2023-07-06 12:04:08 +0900

    ch02_3 끝

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 // ch02_3
 
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-import ClassComponent from './src/screens/ClassComponent';
-import ArrowComponent from './src/screens/ArrowComponent';
+import { SafeAreaView, ScrollView } from 'react-native';
 import Person from './src/screens/Person'
 import * as D from './src/data'
 
-const person = D.createRandomPerson()
+const people = D.makeArray(100).map(D.createRandomPerson)
 
 export default function App() {
+  const children = people.map((person) => (
+    <Person key={person.id} person={person} />
+  ))
+  
   return (
     <SafeAreaView>
-      <ClassComponent />
-      <ArrowComponent />
-      <Person person={person} />
+      <ScrollView>{children}</ScrollView>
     </SafeAreaView>
   )
 }
