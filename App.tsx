3245bd12b8cbc61ea6b05e507fc4ad5dae2ebf71
happyjino <wlsgh1128@naver.com>
2023-07-06 11:55:04 +0900

    잠깐! 타입스크립트

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 // ch02_3
 
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
-import * as D from './src/data'
+import { SafeAreaView } from 'react-native';
 import ClassComponent from './src/screens/ClassComponent';
+import ArrowComponent from './src/screens/ArrowComponent';
+import Person from './src/screens/Person'
+import * as D from './src/data'
 
 const person = D.createRandomPerson()
+
 export default function App() {
   return (
     <SafeAreaView>
       <ClassComponent />
+      <ArrowComponent />
+      <Person person={person} />
     </SafeAreaView>
   )
 }
